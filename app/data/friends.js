4cc39d3b125793d6b54dc452e2526b26bc7d60de
Daniel Moss <dmoss@stratoswp.com>
2018-01-31 23:02:28 -0500

    apiroutes js finished and working to return best match

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,7 +15,7 @@ var friendsArray = [{
     ]
 },
 {
-    "name": "Ahmed2",
+    "name": "Test Guy",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
         2,
@@ -33,16 +33,4 @@ var friendsArray = [{
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArray;
 
-// function arrayLoop() {
-//     for (let i = 0; i < friendsArray.length; i++) {
-//         let scoreArray = friendsArray[i].scores;
-//         console.log(scoreArray);
-//         for (let j = 0; j < scoreArray.length; j++) {
-
-//         }
-//     }
-// }
-
-// arrayLoop();
-// console.log(friendsArray[0].scores);
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,23 +3,67 @@ var friendData = require("../data/friends.js");
 
 
 
-module.exports = function (app) { 
+module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         return res.json(friendData);
     });
-    
+
     app.post("/api/friends", function (req, res) {
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body-parser middleware
         var newFriend = req.body;
-        // Using a RegEx Pattern to remove spaces from newFriend
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-        friendData.push(newFriend);
+        var userName = newFriend.name;
+        var userPhoto = newFriend.photo;
+
+        // convert the 'scores[]' array into an array of integers and assign it to a properly named property on the object
+        newFriend.scores = newFriend['scores[]'].map(function (score) {
+            return parseInt(score);
+        });
+        // delete the badly named property
+        delete newFriend['scores[]'];
+
+        var userScores = newFriend.scores;
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            scoreDif: 41
+        }
+
+
         console.log(newFriend);
         console.log(friendData);
-    
+
+        // Loop through each object in the friendData array
+        for (let i = 0; i < friendData.length; i++) {
+            var totalDif = 0;
+            // Loop through the "score array" for each person in the friend data array
+            for (let j = 0; j < friendData[i].scores.length; j++) {
+                const element = friendData[i].scores;
+
+                // compare the newFriend data to the score array and get a total difference
+                totalDif += Math.abs(parseInt(userScores[j]) - parseInt(element[j]));
+
+                // console.log(totalDif);
+                console.log(userScores[j]);
+                console.log(element[j]);
+                console.log(totalDif);
+
+                // find the exisiting friend with the lowest difference number
+                if (totalDif < bestMatch.scoreDif) {
+                    bestMatch.name = friendData[i].name;
+                    bestMatch.photo = friendData[i].photo;
+                    bestMatch.scoreDif = totalDif;
+                }
+
+            }
+
+
+        }
         // respond with best match
-        // res.json(newcharacter);
+        res.json(bestMatch);
+
+        // push the new friend into friendData array
+        friendData.push(newFriend);
     });
- }
+}
